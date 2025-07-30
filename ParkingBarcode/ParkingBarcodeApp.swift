ee5e881807a7117eb17270f18586758b0bf85e99
euna.song <euna.song@homeplus.co.kr>
2025-07-30 10:48:09 +0900

    Initial Commit

---
+++ ParkingBarcode/ParkingBarcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingBarcodeApp.swift
+//  ParkingBarcode
+//
+//  Created by 송은아 on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkingBarcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
