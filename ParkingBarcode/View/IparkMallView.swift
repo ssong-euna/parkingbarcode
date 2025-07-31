1bd699df702b3e211120fcd950f01a13534746d5
euna.song <euna.song@homeplus.co.kr>
2025-07-31 16:26:30 +0900

    아이파크몰 추가

---
+++ ParkingBarcode/View/IparkMallView.swift
@@ -0,0 +1,18 @@
+//
+//  IparkMallView.swift
+//  ParkingBarcode
+//
+//  Created by 송은아 on 7/31/25.
+//
+
+import SwiftUI
+
+struct IparkMallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IparkMallView()
+}
